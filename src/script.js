94c9a1d67f64fea38e2bb4069a423c51cc1c71cf
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2024-03-08 06:02:41 +0100

    Chapter 1

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ dropdown.addEventListener("click", () => {
    dialog.showModal();
    dropdown.style.transform = "scale(0.9)";
    dropdown.addEventListener("transitionend", () => {
-      dropdown.style.transform = "scale(1)"
+      dropdown.style.transform = "scale(1)";
    });
 });
 
@@ -17,4 +17,20 @@ closeBtn.addEventListener("click", () => {
       dialog.style.animation = "FadeIn 0.2s";
       dialog.removeEventListener("animationend", closeDialog);
    });
+});
+
+const dontOpen = document.querySelector("#dontOpen");
+
+dontOpen.addEventListener("mouseover", () => {
+   dontOpen.style.color = "red";
+   dontOpen.textContent = "Passa al prossimo Capitolo"
+});
+
+dontOpen.addEventListener("mouseout", () => {
+   dontOpen.style.color = "white";
+   dontOpen.textContent = "NON MI APRIRE";
+});
+
+dontOpen.addEventListener("click", () => {
+   window.location.href = "Chapter2.html";
 });
